5824d517c51e454c3fb224a604938c8a90a91b25
Leanid Nasanovich <olen4ixxx@gmail.com>
2021-11-23 15:39:25 +0300

    Updates

---
+++ static/js/main.03f9db06.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-gh-pages"]=this["webpackJsonpreact-gh-pages"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);c(1);var s=c(3),r=c.n(s),n=c(0);var a=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("select",{children:"qwerty"})})};r.a.render(Object(n.jsx)(a,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.03f9db06.chunk.js.map
